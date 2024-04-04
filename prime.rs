8a9272068631240e7fed167a0a8f235b9207ed7d
triptisingla <triptisingla306@gmail.com>
2024-04-04 19:32:42 +0530

    prime

--- prime.rs
+++ prime.rs
@@ -1,14 +1,14 @@
 fn isPrime(n: u32) -> bool {
     if n <= 1 {
-        return false; // 0 and 1 are not prime numbers
+        return false;
     }
     let sqrtOfn = (n as f64).sqrt() as u32;
     for i in 2..=sqrtOfn {
         if n % i == 0 {
-            return false; // n is divisible by i, hence not prime
+            return false; 
         }
     }
-    true // n is not divisible by any number within the range, hence prime
+    true 
 }
 
 fn main() {
