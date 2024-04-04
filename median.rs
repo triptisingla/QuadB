12ec99cc627453b786051d21b92075c998eb3211
triptisingla <triptisingla306@gmail.com>
2024-04-04 19:34:00 +0530

    median

---
+++ median.rs
@@ -0,0 +1,14 @@
+fn medianOfArray(arr: &[i32]) -> f64 {
+    let len = arr.len();
+    if len % 2 == 0 {
+        (arr[len / 2 - 1] + arr[len / 2]) as f64 / 2.0
+    } else {
+        arr[len / 2] as f64
+    }
+}
+
+fn main() {
+    let arr = vec![1, 2, 3, 4, 5, 9];
+    let output = medianOfArray(&arr);
+    println!("Median of {:?} is {}", arr, output);
+}
